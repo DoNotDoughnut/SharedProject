6d8d251714dce1a88382a7750d29443be56f2c7f
Vedant <Vedant@VedantLaptop.lan>
2020-12-27 22:16:05 -0800

    I made changes WOOOOO

--- src/net/project/MainClass.java
+++ src/net/project/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello word!");
 		System.out.println("vedented head moment");
-		
+		System.out.print("WOOOOOOOO");
 	}
 	
 }
