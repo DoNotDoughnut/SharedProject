aa4642da7c6a2926d8d926bf73697bf5f64757a2
Rhys Holloway <rhyswilliamholloway@gmail.com>
2020-12-27 22:36:08 -0800

    test commit from behind

--- src/net/project/MainClass.java
+++ src/net/project/MainClass.java
@@ -5,10 +5,12 @@ import javax.swing.JFrame;
 public class MainClass {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello word!");
 		System.out.println("vedented head moment");
-		System.out.print("WOOOOOOOO");
+		System.out.println("WOOOOOOOO");
 		System.out.println("test");
+		
 		JFrame frame = new JFrame("test");
 		frame.setSize(300, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
