04426396740e4083acf94e56334dee40325301b7
Nareg K <Barkev's Auto@DESKTOP-PD0UAOO.attlocal.net>
2020-12-27 22:33:22 -0800

    whos joe

--- src/net/project/MainClass.java
+++ src/net/project/MainClass.java
@@ -14,6 +14,8 @@ public class MainClass {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		frame.setTitle("Joe Mamma");
+		
 	}
 	
 }
