b39586afb0de0751a784a943784eff85d7f836b7
Rhys Holloway <rhyswilliamholloway@gmail.com>
2020-12-27 22:30:38 -0800

    created a window
    Signed-off-by: Rhys Holloway <rhyswilliamholloway@gmail.com>

--- src/net/project/MainClass.java
+++ src/net/project/MainClass.java
@@ -1,5 +1,7 @@
 package net.project;
 
+import javax.swing.JFrame;
+
 public class MainClass {
 
 	public static void main(String[] args) {
@@ -7,6 +9,11 @@ public class MainClass {
 		System.out.println("vedented head moment");
 		System.out.print("WOOOOOOOO");
 		System.out.println("test");
+		JFrame frame = new JFrame("test");
+		frame.setSize(300, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 	
 }
